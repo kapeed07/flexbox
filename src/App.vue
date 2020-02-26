73936d5ea763c05492cb570adc04e6bfe3507315
Deepak Bhardwaj <deepak@customersuccessbox.com>
2020-02-26 22:38:56 +0530

    add design and basic classes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <center style="font-size: 100px;">FLEXBOX</center>
-    <flexbox></flexbox>
+    <div class="db-full-width db-full-height db-flex db-column db-justify-center">
+      <div class="db-flex db-justify-center">
+        <p class="db-heading">FLEXBOX</p>
+      </div>
+      <flexbox></flexbox>
+    </div>
   </div>
 </template>
 
@@ -17,10 +21,29 @@ export default {
 </script>
 
 <style>
+
+#app {
+  font-family: 'Luckiest Guy' !important;
+}
+
+.db-heading {
+  letter-spacing: 50px;
+  color: #acc8dd;
+  -webkit-text-stroke-width: 3px;
+  -webkit-text-stroke-color: #56A8E4;
+  font-size: 100px;
+}
+.db-full-width {
+  width: 100vw;
+}
+.db-full-height {
+  height: 100vh;
+}
+
 body {
   width: 100vw;
   height: 100vh;
-  background-color: #F9FAFA;
+  background-color: #F0F7FE;
 }
 
 </style>
--- src/components/flexbox.vue
+++ src/components/flexbox.vue
@@ -1,18 +1,34 @@
 <template>
-  <div>
-    <div class="flexbox"
-    :class="[...styles]">
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-    </div>
-    <div style="margin-top: 100px;" class="flex row justify-center">
-        <div class="field">
-            <b-switch @input="justifyContentCenter" :value="justifyContent">
-                Justify Content Center
-            </b-switch>
+  <div class="db-flex db-row db-space-around">
+    <div
+      class="db-flexbox db-border db-position-relative"
+      :class="[...styles, isFlexRow ? 'db-row' : 'db-column']">
+        <div
+          v-for="(box, index) in boxes"
+          :key="index"
+          class="db-box"
+          :class="[ box ? 'db-self db-self-center' : '' ]"
+          @click="markBox(index)">
+        </div>
+        <div class="db-position-absolute" style="transform: rotate(-90deg); left: -65px; top: 100px;">
+          {{ isFlexRow ? 'Cross' : 'Main' }} Axis →
+        </div>
+        <div class="db-position-absolute" style="top: -30px; left: 55px;">
+          {{ isFlexRow ? 'Main' : 'Cross' }} Axis →
         </div>
     </div>
+   <div class="db-console db-border db-flex db-row db-align-center db-justify-center">
+     <div class="db-flex db-column">
+      <ul>
+        <li>Click on any box to make it align-self: center</li>
+      </ul>
+      <div class="field">
+        <b-switch @input="isFlexRow = !isFlexRow" :value="isFlexRow">
+          {{ isFlexRow ? 'Row' : 'Column' }}
+        </b-switch>
+      </div>
+     </div>
+   </div>
   </div>
 </template>
 
@@ -24,63 +40,107 @@ export default {
   },
   data () {
     return {
-      isFlex: false,
-      justifyContent: false,
-      styles: ["flex", "row"]
+      isFlexRow: true,
+      boxes: [0,0,0],
+      styles: ["db-flex"]
     }
   },
   methods: {
     justifyContentCenter() {
-      const index = this.styles.indexOf('justify-center');
+      const index = this.styles.indexOf('db-justify-center');
       if(index >=0 ) {
         this.styles.splice(index, 1)
       } else {
-        this.styles.push('justify-center')
+        this.styles.push('db-justify-center')
       }
       this.justifyContent = !this.justifyContent
     },
+    markBox(index) {
+      let boxes = [...this.boxes]
+      boxes[index] = !boxes[index]
+      this.boxes = boxes;
+    },
     changeFlex() {
-      const index = this.styles.indexOf('flex');
+      const index = this.styles.indexOf('db-flex');
       if(index >=0 ) {
         this.styles.splice(index, 1)
       } else {
-        this.styles.push('flex')
+        this.styles.push('db-flex')
       }
     }
   }
 }
 </script>
 
-<style scoped>
-.flex {
+<style>
+.db-flex {
   display: flex;
 }
 
-.row {
+.db-self-center {
+  align-self: center;
+
+}
+
+.db-space-around {
+  justify-content: space-around;
+}
+
+.db-space-between {
+  justify-content: space-between;
+}
+
+
+
+.db-row {
   flex-direction: row;
 }
 
-.column {
+.db-position-relative {
+  position: relative;
+}
+
+.db-position-absolute {
+  position: absolute;
+}
+
+.db-column {
   flex-direction: column;
 }
 
-.justify-center {
+.db-self {
+  border: 3px dotted red;
+}
+
+.db-justify-center {
   justify-content: center;
 }
 
-.box {
-  background-color: orange;
-  box-shadow: 10px 10px 5px -10px rgba(0,0,0,0.78);
-  width: 100px;
-  margin: 10px;
-  border-radius: 4px;
-  height: 100px;
+.db-align-center {
+  align-items: center;
 }
 
-.flexbox {
+.db-box {
+  background-color: orange;
+  box-shadow: 10px 10px 5px -10px rgba(0,0,0,0.78);
+  width: 50px;
+  height: 50px;
+  margin: 10px;
+  border-radius: 4px;
+}
+
+.db-border {
   border: 3px dashed #56A8E4;
-  width: 60%;
-  height: 80%;
-  margin: 0 auto;
+  border-radius: 4px;
+}
+
+.db-console {
+  width: 30%;
+  height: 600px;
+}
+
+.db-flexbox {
+  width: 50%;
+  height: 600px;
 }
 </style>
