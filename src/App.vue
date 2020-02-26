4b410f419a3dee2aed077c06ca84e038c23ecff9
Deepak Bhardwaj <deepak@customersuccessbox.com>
2020-02-26 15:15:18 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <center style="font-size: 100px;">FLEXBOX</center>
+    <flexbox></flexbox>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Flexbox from './components/flexbox';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Flexbox
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  width: 100vw;
+  height: 100vh;
+  background-color: #F9FAFA;
 }
+
 </style>
---
+++ src/components/flexbox.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="flexbox"
+    :class="[...styles]">
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+    </div>
+    <div style="margin-top: 100px;" class="flex row justify-center">
+        <div class="field">
+            <b-switch @input="justifyContentCenter" :value="justifyContent">
+                Justify Content Center
+            </b-switch>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'flexbox',
+  props: {
+    
+  },
+  data () {
+    return {
+      isFlex: false,
+      justifyContent: false,
+      styles: ["flex", "row"]
+    }
+  },
+  methods: {
+    justifyContentCenter() {
+      const index = this.styles.indexOf('justify-center');
+      if(index >=0 ) {
+        this.styles.splice(index, 1)
+      } else {
+        this.styles.push('justify-center')
+      }
+      this.justifyContent = !this.justifyContent
+    },
+    changeFlex() {
+      const index = this.styles.indexOf('flex');
+      if(index >=0 ) {
+        this.styles.splice(index, 1)
+      } else {
+        this.styles.push('flex')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+
+.row {
+  flex-direction: row;
+}
+
+.column {
+  flex-direction: column;
+}
+
+.justify-center {
+  justify-content: center;
+}
+
+.box {
+  background-color: orange;
+  box-shadow: 10px 10px 5px -10px rgba(0,0,0,0.78);
+  width: 100px;
+  margin: 10px;
+  border-radius: 4px;
+  height: 100px;
+}
+
+.flexbox {
+  border: 3px dashed #56A8E4;
+  width: 60%;
+  height: 80%;
+  margin: 0 auto;
+}
+</style>
