e505b54c1ca1ecd64762493a3cc2329d2848f8a6
Deepak Bhardwaj <deepak@customersuccessbox.com>
2020-02-27 16:58:46 +0530

    add closable icon and some filters

--- src/components/flexbox.vue
+++ src/components/flexbox.vue
@@ -2,13 +2,19 @@
   <div class="db-flex db-row db-space-around">
     <div
       class="db-flexbox db-border db-position-relative"
-      :class="[...styles, isFlexRow ? 'db-row' : 'db-column']">
+      :class="[value,...styles, isFlexRow ? 'db-row' : 'db-column', isFlexWrap ? 'db-flex-wrap' : '']">
         <div
           v-for="(box, index) in boxes"
           :key="index"
-          class="db-box"
-          :class="[ box ? 'db-self db-self-center' : '' ]"
-          @click="markBox(index)">
+          :value="index"
+          class="db-flex db-justify-center db-align-center db-box db-position-relative"
+          :class="[ box ? 'db-self db-self-center' : '']"
+          @click="markBox(index)"
+          >
+            {{index}}
+          <div @click="removeBox(index)" class="db-close db-pointer">
+            <i class="fa fa-times"></i>
+          </div>
         </div>
         <div class="db-position-absolute" style="transform: rotate(-90deg); left: -65px; top: 100px;">
           {{ isFlexRow ? 'Cross' : 'Main' }} Axis →
@@ -19,14 +25,27 @@
     </div>
    <div class="db-console db-border db-flex db-row db-align-center db-justify-center">
      <div class="db-flex db-column">
+       <template>
+        <el-select v-model="value" @change="changeJustify" placeholder="Select">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </template>
       <ul>
+        <li><b-button type="is-primary" @click="addBox">Click to add box</b-button></li>
         <li>Click on any box to make it align-self: center</li>
       </ul>
-      <div class="field">
+      
         <b-switch @input="isFlexRow = !isFlexRow" :value="isFlexRow">
           {{ isFlexRow ? 'Row' : 'Column' }}
         </b-switch>
-      </div>
+        <b-switch @input="isFlexWrap = !isFlexWrap" :value="isFlexWrap">
+          {{ isFlexWrap ? 'Wrap' : '' }}
+        </b-switch>
      </div>
    </div>
   </div>
@@ -40,20 +59,49 @@ export default {
   },
   data () {
     return {
+      options: [
+        {
+          value: 'db-justify-start',
+          label: 'flex-start'
+        },
+        {
+          value: 'db-justify-center',
+          label: 'flex-center'
+        },
+        {
+          value: 'db-justify-end',
+          label: 'flex-end'
+        }
+        ],
+        value: '',
       isFlexRow: true,
+      isFlexWrap: false,
       boxes: [0,0,0],
       styles: ["db-flex"]
     }
   },
   methods: {
-    justifyContentCenter() {
-      const index = this.styles.indexOf('db-justify-center');
-      if(index >=0 ) {
-        this.styles.splice(index, 1)
-      } else {
-        this.styles.push('db-justify-center')
-      }
-      this.justifyContent = !this.justifyContent
+    removeBox(index) {
+      this.boxes.splice(index, 1);
+    },
+    addBox() {
+      this.boxes.push(0);
+    },
+    changeJustify() {
+      // let styles = [...this.styles]
+      // if(styles.length === 1) {
+      //   styles.push();
+      // } else {
+      //   styles.pop();
+      //   styles.push(value);
+      // }
+      // this.styles = styles;
+      // const index = this.styles.indexOf(`db-justify-${value}`);
+      // if(index >=0 ) {
+      //   this.styles.splice(index, 1)
+      // } else {
+      //   this.styles.push(`db-justify-${value}`)
+      // }
     },
     markBox(index) {
       let boxes = [...this.boxes]
@@ -91,7 +139,6 @@ export default {
 }
 
 
-
 .db-row {
   flex-direction: row;
 }
@@ -114,6 +161,14 @@ export default {
 
 .db-justify-center {
   justify-content: center;
+}
+
+.db-justify-start {
+  justify-content: flex-start;
+}
+
+.db-justify-end {
+  justify-content: flex-end;
 }
 
 .db-align-center {
@@ -143,4 +198,22 @@ export default {
   width: 50%;
   height: 600px;
 }
+
+.db-close {
+  width: 15px;
+  font-size: 12px;
+  height: 15px;
+  position: absolute;
+  right: 0px;
+  color: white;
+  top: 3px;
+}
+
+.db-flex-wrap {
+  flex-wrap: wrap;
+}
+ 
+ .db-pointer {
+   cursor: pointer;
+ }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import { Button, Select } from 'element-ui'
+
+// import components
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
+Vue.use(ElementUI, { locale })
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
